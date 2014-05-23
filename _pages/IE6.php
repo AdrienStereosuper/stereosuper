c1f178c940c2fc4d23d9b1d328b251113e2f2a29
Adrien <adrien.le.mentheour@gmail.com>
2014-05-23 09:10:04 +0200

    Premier import fichiers

---
+++ _pages/IE6.php
@@ -0,0 +1,5 @@
+<div id="content" class="pageIe6">
+	<h2>Pardonnez nous.<span></span></h2>
+	<p>Notre site n’est pas optimisé pour Internet Explorer 6 (sorti en 2001).</p><br />
+	<p>Nous vous invitons, si vous le pouvez, <a href="http://www.mozilla-europe.org/fr/firefox/" rel="external" title="Mozilla Firefox">à télécharger un navigateur plus récent.</a></p>
+</div>
