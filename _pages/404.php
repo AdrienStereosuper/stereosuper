a90ff6c1c720a647b1f31d33f7b007842fef2fa8
Adrien <adrien.le.mentheour@gmail.com>
2014-05-23 10:03:57 +0200

    Commit par type de fichier / index.html

--- _pages/404.php
+++
@@ -1,4 +0,0 @@
-<div id="content" class="e404">
-	<h2>Cette page n'existe pas (ou plus, depuis 1989).<span></span></h2>
-	<h3 id="retouraccueil">Nous vous invitons à repartir de la <a href="accueil.html"> page d'accueil</a></h3>
-</div>
--- _pages/accueil.php
+++
@@ -1,5 +0,0 @@
-<div id="content" class="home">
-	<h2>Finis, les sites mononuls !<span></span></h2>
-	<p>Vos objectifs ne sont pas virtuels.</p><br />
-	<p>Nous adorons vous aider &agrave; les atteindre gr&acirc;ce &agrave; une communication cr&eacute;ative sur Internet.</p><br />
-</div>
